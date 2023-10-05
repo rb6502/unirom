d6f23502835307afc01e2666d7e755d4d738de6b
arbee <rb6502@users.noreply.github.com>
2023-10-04 23:23:12 -0400

    Fixed up spaces/tabs fighting with MAME's srcclean utility.

--- unirom.cpp
+++ unirom.cpp
@@ -1,11 +1,11 @@
 /*
-	unirom.cpp: decode Universal product info tables in Old World Mac ROMs
-	By R. Belmont
-	license:BSD-3-Clause
+    unirom.cpp: decode Universal product info tables in Old World Mac ROMs
+    By R. Belmont
+    license:BSD-3-Clause
 
-	This is catastrophically gross code but it does what I need.
+    This is catastrophically gross code but it does what I need.
 
-	To build: clang++ -o unirom -O3 unirom.cpp
+    To build: clang++ -o unirom -O3 unirom.cpp
 */
 
 #include <stdio.h>
@@ -21,20 +21,20 @@
 // box flag = gestalt ID - 6 for most of these machines
 static const char *boxnames[128] =
 {
-    /* 0-15 */    "II", "IIx", "IIcx", "SE/30", "Portable", "IIci", "Four Square", "IIfx", "Aurora CX16", "Aurora SE25", "Aurora SE16", "Classic", "IIsi", "LC", "Quadra 900", "PowerBook 170",
-    /* 16-31 */   "Quadra 700", "Classic II", "PowerBook 100", "PowerBook 140", "Quadra 950", "LC III", "Sun MAE or Carnation 33", "PowerBook Duo 210", "Centris 650", "Columbia", "PowerBook Duo 230", "PowerBook 180", "PowerBook 160", "Quadra 800", "Quadra 650", "LC II",
-    /* 32-47 */   "PowerBook Duo 250", "DBLite 20", "Vail 16", "PowerMac 5200 (was Carnation 25)", "PowerMac 6200 (was Carnation 16)", "Cyclone 33", "Brazil 16L", "IIvx", "Brazil 16F", "Brazil 32F", "Brazil C", "Color Classic", "PowerBook 180c", "Wombat 40", "Centris 610", "Quadra 610",
-    /* 48-63 */   "PowerBook 145", "Brazil 32cF", "LC 520", "Unused", "Wombat 20", "Wombat 40F", "Centris 660AV", "88100 or Quadra 700 w/RISC Card", "LC III+", "WLCD33", "PowerMac 8100", "PDM 80F", "PDM 100F", "PowerMac 7500", "PowerMac 7300", "PowerMac 7600/8600/9600",
-    /* 64-79 */   "LC 930", "Hokusai", "PowerBook 5x0", "Pippin", "PDM Evt1", "PowerMac 6100", "Yeager FSTN", "PowerBook Duo 270c", "Quadra 840AV", "Tempest 33", "LC 550", "Color Classic", "Centris 650 w/RISC Card", "Color Classic II", "PowerBook 165", "PowerBook 190",
-    /* 80-95 */   "LC475 20 MHz", "Optimus 20", "Mac TV", "LC 475", "LC 475 33 MHz", "Optimus 25", "LC 575", "Quadra 605 20 MHz", "Quadra 605", "Quadra 605 33 MHz", "Malcolm 25", "Malcolm 33", "Quadra 630/LC 630", "Tell", "PDM 66WLCD", "PDM 80WLCD",
-    /* 96-111 */  "PowerBook Duo 280", "PowerBook Duo 280c", "Quadra 900 w/RISC Card", "Quadra 950 w/RISC Card", "Centris 610 w/RISC Card", "Quadra 800 w/RISC Card", "Quadra 610 w/RISC Card", "Quadra 650 w/RISC Card", "Tempest w/RISC Card", "PDM 50L", "PowerMac 7100", "PDM 80L", "Blackbird BFD", "PowerBook 150", "Quadra 700 w/STP", "Quadra 900 w/STP",
-    /* 112-127 */ "Quadra 950 w/STP", "Centris 610 w/STP", "Centris 650 w/STP", "Quadra 610 w/STP", "Quadra 650 w/STP", "Quadra 800 w/STP", "PowerBook Duo 2300", "AJ 80", "PowerBook 5x0 PowerPC upgrade", "Malcolm BB80", "PowerBook 5300", "M2 80", "MAE on HP/UX", "MAE on AIX", "MAE on AUX", "Extended"
+	/* 0-15 */    "II", "IIx", "IIcx", "SE/30", "Portable", "IIci", "Four Square", "IIfx", "Aurora CX16", "Aurora SE25", "Aurora SE16", "Classic", "IIsi", "LC", "Quadra 900", "PowerBook 170",
+	/* 16-31 */   "Quadra 700", "Classic II", "PowerBook 100", "PowerBook 140", "Quadra 950", "LC III", "Sun MAE or Carnation 33", "PowerBook Duo 210", "Centris 650", "Columbia", "PowerBook Duo 230", "PowerBook 180", "PowerBook 160", "Quadra 800", "Quadra 650", "LC II",
+	/* 32-47 */   "PowerBook Duo 250", "DBLite 20", "Vail 16", "PowerMac 5200 (was Carnation 25)", "PowerMac 6200 (was Carnation 16)", "Cyclone 33", "Brazil 16L", "IIvx", "Brazil 16F", "Brazil 32F", "Brazil C", "Color Classic", "PowerBook 180c", "Wombat 40", "Centris 610", "Quadra 610",
+	/* 48-63 */   "PowerBook 145", "Brazil 32cF", "LC 520", "Unused", "Wombat 20", "Wombat 40F", "Centris 660AV", "88100 or Quadra 700 w/RISC Card", "LC III+", "WLCD33", "PowerMac 8100", "PDM 80F", "PDM 100F", "PowerMac 7500", "PowerMac 7300", "PowerMac 7600/8600/9600",
+	/* 64-79 */   "LC 930", "Hokusai", "PowerBook 5x0", "Pippin", "PDM Evt1", "PowerMac 6100", "Yeager FSTN", "PowerBook Duo 270c", "Quadra 840AV", "Tempest 33", "LC 550", "Color Classic", "Centris 650 w/RISC Card", "Color Classic II", "PowerBook 165", "PowerBook 190",
+	/* 80-95 */   "LC475 20 MHz", "Optimus 20", "Mac TV", "LC 475", "LC 475 33 MHz", "Optimus 25", "LC 575", "Quadra 605 20 MHz", "Quadra 605", "Quadra 605 33 MHz", "Malcolm 25", "Malcolm 33", "Quadra 630/LC 630", "Tell", "PDM 66WLCD", "PDM 80WLCD",
+	/* 96-111 */  "PowerBook Duo 280", "PowerBook Duo 280c", "Quadra 900 w/RISC Card", "Quadra 950 w/RISC Card", "Centris 610 w/RISC Card", "Quadra 800 w/RISC Card", "Quadra 610 w/RISC Card", "Quadra 650 w/RISC Card", "Tempest w/RISC Card", "PDM 50L", "PowerMac 7100", "PDM 80L", "Blackbird BFD", "PowerBook 150", "Quadra 700 w/STP", "Quadra 900 w/STP",
+	/* 112-127 */ "Quadra 950 w/STP", "Centris 610 w/STP", "Centris 650 w/STP", "Quadra 610 w/STP", "Quadra 650 w/STP", "Quadra 800 w/STP", "PowerBook Duo 2300", "AJ 80", "PowerBook 5x0 PowerPC upgrade", "Malcolm BB80", "PowerBook 5300", "M2 80", "MAE on HP/UX", "MAE on AIX", "MAE on AUX", "Extended"
 };
 
 static const char *decoders[25] =
 {
-    "Unknown", "PALs", "BBU", "Normandy", "Mac II Glue", "MDU", "FMC", "V8/Eagle/Spice", "Orwell", "Jaws", "MSC", "Sonora/Ardbeg/Tinker Bell", "Niagra", "YMCA or djMEMC/MEMCjr/F108",
-    "djMEMC/MEMCjr/F108", "HMC", "Pratt", "Hammerhead", "Tinker Bell", "19", "20", "21", "22", "23", "Grackle"
+	"Unknown", "PALs", "BBU", "Normandy", "Mac II Glue", "MDU", "FMC", "V8/Eagle/Spice", "Orwell", "Jaws", "MSC", "Sonora/Ardbeg/Tinker Bell", "Niagra", "YMCA or djMEMC/MEMCjr/F108",
+	"djMEMC/MEMCjr/F108", "HMC", "Pratt", "Hammerhead", "Tinker Bell", "19", "20", "21", "22", "23", "Grackle"
 };
 
 static const char *decoder_regs[45] =
@@ -94,51 +94,51 @@ static FILE *file;
 
 unsigned int read8()
 {
-    unsigned char temp;
+	unsigned char temp;
 
-    fread(&temp, 1, 1, file);
+	fread(&temp, 1, 1, file);
 
-    return temp;
+	return temp;
 }
 
 unsigned int read16()
 {
-    unsigned char data[2];
+	unsigned char data[2];
 
-    fread(&data, 2, 1, file);
+	fread(&data, 2, 1, file);
 
-    return data[0]<<8 | data[1];
+	return data[0]<<8 | data[1];
 }
 
 unsigned int read32()
 {
-    unsigned char data[4];
+	unsigned char data[4];
 
-    fread(&data, 4, 1, file);
+	fread(&data, 4, 1, file);
 
-    return data[0]<<24 | data[1]<<16 | data[2]<<8 | data[3];
+	return data[0]<<24 | data[1]<<16 | data[2]<<8 | data[3];
 }
 
 void seek32(unsigned int offset)
 {
-    fseek(file, offset, SEEK_SET);
+	fseek(file, offset, SEEK_SET);
 }
 
 void parse_table(unsigned int tblOffs, unsigned int boxOffs, unsigned int viaOffs, unsigned int idOffs)
 {
-    unsigned int entryPtr, infoPtr, tblPos;
+	unsigned int entryPtr, infoPtr, tblPos;
 
-    tblPos = tblOffs;
+	tblPos = tblOffs;
 
-    seek32(tblPos);
-    tblPos += 4;
-    entryPtr = read32();
-    while (entryPtr != 0)
-    {
-        unsigned int box, decoder;
-        unsigned int ID, via1, via2;
+	seek32(tblPos);
+	tblPos += 4;
+	entryPtr = read32();
+	while (entryPtr != 0)
+	{
+		unsigned int box, decoder;
+		unsigned int ID, via1, via2;
 
-        infoPtr = entryPtr + (tblPos - 4);
+		infoPtr = entryPtr + (tblPos - 4);
 
 		seek32(infoPtr);
 		const unsigned int decoderOffs = read32();
@@ -147,40 +147,40 @@ void parse_table(unsigned int tblOffs, unsigned int boxOffs, unsigned int viaOff
 		const int videoOffs = (int)read32();
 		unsigned int videoAddr = infoPtr + videoOffs;
 
-        seek32(infoPtr+boxOffs);
-        box = read8();
-        decoder = read8();
+		seek32(infoPtr+boxOffs);
+		box = read8();
+		decoder = read8();
 
-        seek32(infoPtr+viaOffs);
-        via1 = read32();
-        via2 = read32();
+		seek32(infoPtr+viaOffs);
+		via1 = read32();
+		via2 = read32();
 
-        seek32(infoPtr+idOffs);
-        ID = read16();
+		seek32(infoPtr+idOffs);
+		ID = read16();
 
 		bool bShowedSomething = false;
-        if (box <= 127)
-        {
+		if (box <= 127)
+		{
 			bShowedSomething = true;
-            if (decoder <= 24)
-            {
-                printf("[info %x] Box %s decoder %d (%s) VIA mask %08x VIA match %08x ID %04x\n", infoPtr, boxnames[box], decoder, decoders[decoder], via1, via2, ID);
-            }
-            else
-            {
-                printf("[info %x] Box %s decoder %d VIA mask %08x VIA match %08x ID %04x\n", infoPtr, boxnames[box], decoder, via1, via2, ID);
-            }
-        }
-        else if (box == 0xfe)
-        {
+			if (decoder <= 24)
+			{
+				printf("[info %x] Box %s decoder %d (%s) VIA mask %08x VIA match %08x ID %04x\n", infoPtr, boxnames[box], decoder, decoders[decoder], via1, via2, ID);
+			}
+			else
+			{
+				printf("[info %x] Box %s decoder %d VIA mask %08x VIA match %08x ID %04x\n", infoPtr, boxnames[box], decoder, via1, via2, ID);
+			}
+		}
+		else if (box == 0xfe)
+		{
 			bShowedSomething = true;
-            printf("[info %x] Box Plus decoder %d VIA mask %08x VIA match %08x\n", infoPtr, decoder, via1, via2);
-        }
-        else if (box == 0xff)
-        {
+			printf("[info %x] Box Plus decoder %d VIA mask %08x VIA match %08x\n", infoPtr, decoder, via1, via2);
+		}
+		else if (box == 0xff)
+		{
 			bShowedSomething = true;
-            printf("[info %x] Box SE decoder %d VIA mask %08x VIA match %08x\n", infoPtr, decoder, via1, via2);
-        }
+			printf("[info %x] Box SE decoder %d VIA mask %08x VIA match %08x\n", infoPtr, decoder, via1, via2);
+		}
 
 		int itemCount = 0;
 		if (bShowedSomething)
@@ -222,35 +222,35 @@ void parse_table(unsigned int tblOffs, unsigned int boxOffs, unsigned int viaOff
 			printf("\n");
 		}
 
-        seek32(tblPos);
-        tblPos += 4;
-        entryPtr = read32();
-    }
+		seek32(tblPos);
+		tblPos += 4;
+		entryPtr = read32();
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    int i;
-    unsigned int romID;
-    unsigned int tableOffset = 0, tableOffset2 = 0, boxOffs = 0, viaOffs = 0, idOffs = 0;
+	int i;
+	unsigned int romID;
+	unsigned int tableOffset = 0, tableOffset2 = 0, boxOffs = 0, viaOffs = 0, idOffs = 0;
 
-    if (argc < 2)
-    {
+	if (argc < 2)
+	{
 		fprintf(stderr, "UniROM: Old World Mac Universal ROM explorer v1.0\n");
 		fprintf(stderr, "By R. Belmont 2009-2023.  License: BSD-3-clause.\n");
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
-        return -1;
-    }
+		fprintf(stderr, "Usage: %s filename\n", argv[0]);
+		return -1;
+	}
 
-    file = fopen(argv[1], "rb");
+	file = fopen(argv[1], "rb");
 	if (!file)
 	{
 		fprintf(stderr, "ERROR: Unable to open %s\n", argv[1]);
 		return -1;
 	}
 
-    romID = read32();
-    printf("ROM ID is %08x\n", romID);
+	romID = read32();
+	printf("ROM ID is %08x\n", romID);
 
 	i = 0;
 	while (rom_offsets[i] != 0xffffffff)
@@ -276,16 +276,16 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-    if (tableOffset != 0)
-    {
-        parse_table(tableOffset, boxOffs, viaOffs, idOffs);
-    }
+	if (tableOffset != 0)
+	{
+		parse_table(tableOffset, boxOffs, viaOffs, idOffs);
+	}
 
-    if (tableOffset2 != 0)
-    {
-        parse_table(tableOffset2, boxOffs, viaOffs, idOffs);
-    }
+	if (tableOffset2 != 0)
+	{
+		parse_table(tableOffset2, boxOffs, viaOffs, idOffs);
+	}
 
-    fclose(file);
-    return 0;
+	fclose(file);
+	return 0;
 }
